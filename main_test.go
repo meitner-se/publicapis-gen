0d8c32b808f3a845a208358dc78e75ec97d5f81c
simonjanss <simon.jansson1@hotmail.com>
2025-09-06 17:01:12 +0200

    add main-test

--- main_test.go
+++ main_test.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"log/slog"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func Test_main(t *testing.T) {
+	t.Cleanup(func() {
+		slog.SetDefault(slog.Default())
+	})
+
+	buf := new(bytes.Buffer)
+
+	slog.SetDefault(slog.New(slog.NewTextHandler(buf, nil)))
+
+	main()
+
+	assert.Contains(t, buf.String(), "level=ERROR msg=\"failed to run\" error=\"not implemented\"\n")
+}
 
 func Test_run(t *testing.T) {
 	ctx := context.Background()
